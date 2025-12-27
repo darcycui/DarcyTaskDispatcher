6d7d6434dcc168d66fafb58a0191436571917c7e
darcymac <darcymac@tom.com>
2025-12-27 22:12:11 +0800

    优化：消费者 使用receive() 从channel中获取任务

--- src/main/kotlin/executor/noreturn/SerialNoReturnExecutor.kt
+++ src/main/kotlin/executor/noreturn/SerialNoReturnExecutor.kt
@@ -1,32 +1,33 @@
 package executor.noreturn
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.launch
 import task.noreturn.INoReturnValueTask
 
-class SerialNoReturnExecutor private constructor(): INoReturnExecutor {
+class SerialNoReturnExecutor private constructor() : INoReturnExecutor {
     private val scope: CoroutineScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
     private val taskChannel = Channel<INoReturnValueTask>(Channel.UNLIMITED)
+
     companion object {
         private var instance: SerialNoReturnExecutor? = null
         fun getInstance(): SerialNoReturnExecutor {
             return instance
-                    ?: SerialNoReturnExecutor().also {
-                instance = it
-            }
+                ?: SerialNoReturnExecutor().also {
+                    instance = it
+                }
         }
     }
+
     init {
         // 启动消费者协程
         startConsumer()
     }
+
     private fun startConsumer() {
         scope.launch() {
             // 消费者：从队列中取出任务并执行
-            for (task in taskChannel) {
+            while (isActive) {
+                val task = taskChannel.receive()
                 println("${task.getTaskName()} 在线程: ${Thread.currentThread().name} 中执行...")
                 task.onRun()
             }
